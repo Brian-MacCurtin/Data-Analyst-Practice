a80b5d4bd2ec2d7250073bb2e274a773050aa2ce
Brian MacCurtin <brian@marketacumen.com>
2025-05-12 14:17:47 -0400

    wrote a query to find top skills required for data analyst roles

--- SQL Data Analyst Project/Q3_top_demanded_skills.sql
+++ SQL Data Analyst Project/Q3_top_demanded_skills.sql
@@ -1,1 +1,28 @@
 -- RESEARCH QUESTION #3: What are the most in-demand skills for a data analyst?
+
+-- @block
+
+WITH data_analyst_jobs AS (
+    SELECT * 
+    FROM
+        job_postings_fact 
+    WHERE
+        job_title_short = 'Data Analyst'
+)
+
+SELECT 
+    count(*) AS num_jobs,
+    sd.skills
+FROM
+    data_analyst_jobs daj
+LEFT JOIN skills_job_dim sjd
+    ON sjd.job_id = daj.job_id
+LEFT JOIN skills_dim sd
+    ON sd.skill_id = sjd.skill_id
+WHERE 
+    sd.skills IS NOT Null
+GROUP BY
+    sd.skills
+ORDER BY 
+    num_jobs DESC
+    
