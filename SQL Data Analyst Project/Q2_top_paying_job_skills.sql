39a6c21808c9c19df9b0a1f2dcf2e2c6e9ff9941
Brian MacCurtin <brian@marketacumen.com>
2025-05-12 14:05:23 -0400

    wrote query to find skills required for top 10 paying jobs

--- SQL Data Analyst Project/Q2_top_paying_job_skills.sql
+++ SQL Data Analyst Project/Q2_top_paying_job_skills.sql
@@ -32,3 +32,43 @@ LEFT JOIN skills_job_dim sjd
     ON t10.job_id = sjd.job_id
 LEFT JOIN skills_dim sd 
     ON sd.skill_id = sjd.skill_id
+
+
+
+-- Most wanted skills for top 10 paying remote jobs
+-- @block
+WITH top_10_jobs AS (
+    SELECT 
+        cd.name AS company_name,
+        jpf.job_title AS title,
+        jpf.job_id,
+        jpf.job_location AS location,
+        jpf.salary_year_avg AS yearly_salary,
+        jpf.job_schedule_type AS contract_type,
+        jpf.job_posted_date::DATE AS date
+    FROM 
+        job_postings_fact jpf
+    LEFT JOIN company_dim cd
+        ON jpf.company_id = cd.company_id
+    WHERE 
+        jpf.job_title_short = 'Data Analyst' AND 
+        jpf.job_location = 'Anywhere' AND
+        jpf.salary_year_avg IS NOT NULL
+    ORDER BY
+        jpf.salary_year_avg DESC
+    LIMIT 10
+)
+
+SELECT 
+    count(*) AS num_jobs,
+    sd.skills
+FROM 
+    top_10_jobs t10
+LEFT JOIN skills_job_dim sjd
+    ON t10.job_id = sjd.job_id
+LEFT JOIN skills_dim sd 
+    ON sd.skill_id = sjd.skill_id
+GROUP BY
+    sd.skills
+ORDER BY
+    num_jobs DESC
