cc116a31b87c2f7ac3c56bae0b450a4af7179c98
Brian MacCurtin <brian@marketacumen.com>
2025-05-12 15:34:41 -0400

    chaged query so it didn't have duplicated skills from the previous duplicated company query

--- SQL Data Analyst Project/Q2_top_paying_job_skills.sql
+++ SQL Data Analyst Project/Q2_top_paying_job_skills.sql
@@ -70,27 +70,68 @@ ORDER BY
     num_jobs DESC
 
 
+-- Finding all skills associated with the top 10 paying data analyst jobs in Philadelphia, PA
+-- @block
+WITH top_10_jobs AS (
+    SELECT *
+    FROM (
+        SELECT DISTINCT ON (cd.name)
+            jpf.job_id,
+            cd.name AS company_name,
+            jpf.job_title AS title,
+            jpf.job_location AS location,
+            jpf.salary_year_avg AS yearly_salary,
+            jpf.job_schedule_type AS contract_type,
+            jpf.job_posted_date::DATE AS date
+        FROM 
+            job_postings_fact jpf
+        LEFT JOIN company_dim cd ON jpf.company_id = cd.company_id
+        WHERE 
+            jpf.job_title_short = 'Data Analyst' AND 
+            jpf.job_location = 'Philadelphia, PA' AND
+            jpf.salary_year_avg IS NOT NULL
+        ORDER BY 
+            cd.name, jpf.salary_year_avg DESC
+    ) sub
+    ORDER BY yearly_salary DESC
+    LIMIT 10
+)
 
+SELECT 
+    t10.*,
+    sd.skills
+FROM 
+    top_10_jobs t10
+INNER JOIN skills_job_dim sjd
+    ON t10.job_id = sjd.job_id
+INNER JOIN skills_dim sd 
+    ON sd.skill_id = sjd.skill_id
 
 
 -- Most frequently required skills for the top 10 paying data analyst jobs in Philadelphia, PA
 -- @block
 WITH top_10_jobs AS (
-    SELECT 
-        cd.name AS company_name,
-        jpf.job_title AS title,
-        jpf.job_id,
-        jpf.salary_year_avg AS yearly_salary
-    FROM 
-        job_postings_fact jpf
-    LEFT JOIN company_dim cd
-        ON jpf.company_id = cd.company_id
-    WHERE 
-        jpf.job_title_short = 'Data Analyst' AND 
-        jpf.job_location = 'Philadelphia, PA' AND
-        jpf.salary_year_avg IS NOT NULL
-    ORDER BY
-        jpf.salary_year_avg DESC
+    SELECT *
+    FROM (
+        SELECT DISTINCT ON (cd.name)
+            jpf.job_id,
+            cd.name AS company_name,
+            jpf.job_title AS title,
+            jpf.job_location AS location,
+            jpf.salary_year_avg AS yearly_salary,
+            jpf.job_schedule_type AS contract_type,
+            jpf.job_posted_date::DATE AS date
+        FROM 
+            job_postings_fact jpf
+        LEFT JOIN company_dim cd ON jpf.company_id = cd.company_id
+        WHERE 
+            jpf.job_title_short = 'Data Analyst' AND 
+            jpf.job_location = 'Philadelphia, PA' AND
+            jpf.salary_year_avg IS NOT NULL
+        ORDER BY 
+            cd.name, jpf.salary_year_avg DESC
+    ) sub
+    ORDER BY yearly_salary DESC
     LIMIT 10
 )
 
