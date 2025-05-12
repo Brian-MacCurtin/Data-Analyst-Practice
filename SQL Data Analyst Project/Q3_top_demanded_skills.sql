e5b7325d898d6b494fe1548c8b6f450fb2b7257d
Brian MacCurtin <brian@marketacumen.com>
2025-05-12 17:02:17 -0400

    cleaned up syntax and limited results to top 25 skills

--- SQL Data Analyst Project/Q3_top_demanded_skills.sql
+++ SQL Data Analyst Project/Q3_top_demanded_skills.sql
@@ -1,28 +1,21 @@
 -- RESEARCH QUESTION #3: What are the most in-demand skills for a data analyst?
 
 -- @block
-
-WITH data_analyst_jobs AS (
-    SELECT * 
-    FROM
-        job_postings_fact 
-    WHERE
-        job_title_short = 'Data Analyst'
-)
-
 SELECT 
     count(*) AS num_jobs,
     sd.skills
 FROM
-    data_analyst_jobs daj
-LEFT JOIN skills_job_dim sjd
-    ON sjd.job_id = daj.job_id
-LEFT JOIN skills_dim sd
+    job_postings_fact jpf
+INNER JOIN skills_job_dim sjd
+    ON sjd.job_id = jpf.job_id
+INNER JOIN skills_dim sd
     ON sd.skill_id = sjd.skill_id
 WHERE 
+    jpf.job_title_short = 'Data Analyst' AND
     sd.skills IS NOT Null
 GROUP BY
     sd.skills
 ORDER BY 
     num_jobs DESC
+LIMIT 25
     
